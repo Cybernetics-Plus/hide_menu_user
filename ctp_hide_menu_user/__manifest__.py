1475485796b6f7dd72404416d8b1868833327d8c
BOMEN <bomen21x@gmail.com>
2022-11-21 01:09:22 +0700

    Update __manifest__.py

--- ctp_hide_menu_user/__manifest__.py
+++ ctp_hide_menu_user/__manifest__.py
@@ -7,31 +7,31 @@
 ###################################################################################
 
 {
-    'name': 'Hide Menu for User',
-    'version': '15.0.1.0.1',
-    'summary': """ 
+    "name": "Hide Menu for User",
+    "version": "15.0.1.0.1",
+    "summary": """ 
             Cybernetics Plus Tools Hide Menu for User
             .""",
-    'description': """ 
+    "description": """ 
             Cybernetics Plus Tools Hide Menu for User
             .""",
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'company': 'Cybernetics Plus Co., Ltd.',
-    'maintainer': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.png'],
-    'category': 'Tools',
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics Plus",
+    "company": "Cybernetics Plus Co., Ltd.",
+    "maintainer": "Cybernetics Plus Co., Ltd.",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.png"],
+    "category": "Tools",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
     ],
-    'depends': ['base'],
-    'data': [
-        'views/res_users.xml',
-        'views/res_security.xml'
+    "depends": ["base"],
+    "data": [
+        "views/res_users.xml",
+        "views/res_security.xml"
     ],
 }
